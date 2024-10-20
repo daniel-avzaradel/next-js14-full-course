e927b600733410a906d1377305dc946ebd52834d
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-20 05:14:15 +0300

    adding routes, layout and outlet

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { RouterProvider , createBrowserRouter} from 'react-router-dom'
+
 import App from './App.jsx'
+import NewPost from './components/NewPost.jsx'
+import RootLayout from './routes/RootLayout.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <RootLayout />, children: [
+    { path: '/', element: <App />},
+    { path: '/create-post', element: <NewPost />},
+  ]},
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

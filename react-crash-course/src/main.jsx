d1c957c4bca254c84eb7c49861c56d99906b5ee1
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-24 09:58:58 +0300

    adding post details

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
         loader: postsLoader,
         children: [
           { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: '/:id', }
         ],
       },
     ],

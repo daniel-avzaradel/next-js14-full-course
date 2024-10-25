ee8344eb351d0da80edc39bbb0d04bddc20fbaab
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-25 09:33:00 +0300

    dynamic routes

--- 01-starting-project/app/blog/[slug]/page.js
+++ 01-starting-project/app/blog/[slug]/page.js
@@ -1,13 +1,11 @@
-import Link from "next/link"
 
-const BlogPage = () => {
+const BlogPostPage = ({ params }) => {
   return (
     <main>
-      <h1>The Blog</h1>
-      <p><Link href={"/blog/post-1"}>Post 1</Link></p>
-      <p><Link href={"/blog/post-2"}>Post 2</Link></p>
+      <h1>Blog Post</h1>
+      <p>{params.slug}</p>
     </main>
   )
 }
 
-export default BlogPage
+export default BlogPostPage

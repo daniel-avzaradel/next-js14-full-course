51c82bb1187a2c1aef341a2d281f3d0facefadef
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-26 23:39:21 +0300

    populating the starting page content

--- 05-onwards-foodies-starting-project/app/page.jsx
+++ 05-onwards-foodies-starting-project/app/page.jsx
@@ -2,21 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <main>
-        <p>
-          <Link href={"/meals"}>Meals</Link>
-        </p>
-        <p>
-          <Link href={"/community"}>Community</Link>
-        </p>
-        <p>
-          <Link href={"/meals/share"}>Share</Link>
-        </p>
-      </main>
-    </main>
+    <>
+    <header></header>
+    <main></main>
+    </>
   );
 }

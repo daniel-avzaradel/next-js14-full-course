f49596172b8a4f7f125f0cae1a4fdd02163bbec7
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-28 20:39:08 +0200

    changed delay for slider

--- 05-onwards-foodies-starting-project/app/components/images/image-slideshow.jsx
+++ 05-onwards-foodies-starting-project/app/components/images/image-slideshow.jsx
@@ -30,7 +30,7 @@ export default function ImageSlideshow() {
       setCurrentImageIndex(((prevIndex) =>
         prevIndex < images.length - 1 ? prevIndex + 1 : 0
       ));
-    }, 1000);
+    }, 1500);
 
     return () => clearInterval(interval);
   }, []);

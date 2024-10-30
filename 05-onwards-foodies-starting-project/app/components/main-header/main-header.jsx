7896b2b2c5e02a9e99144082c538f4a8d4ebf951
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-30 23:18:51 +0200

    community page

--- 05-onwards-foodies-starting-project/app/components/main-header/main-header.jsx
+++ 05-onwards-foodies-starting-project/app/components/main-header/main-header.jsx
@@ -1,17 +1,12 @@
-"use client"
-
 import Link from "next/link";
 import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
-
-  let path = usePathname()
-
   return (
     <>
       <MainHeaderBackground />
@@ -28,10 +23,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community" className={path = '/community' ? classes.active : undefined}>Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>

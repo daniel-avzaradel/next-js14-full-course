e851f9cb8ee961637ebe815e80c62897c701aeb2
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-10-30 23:28:03 +0200

    adding meals

--- 05-onwards-foodies-starting-project/app/meals/page.jsx
+++ 05-onwards-foodies-starting-project/app/meals/page.jsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
+import classes from './page.module.css'
 
-const Meals = () => {
+const MealsPage = () => {
   return (
-    <main>
-      <h1>Meals</h1>
-      <p>
-        <Link href="/meals/hamburguer">Hamburguer</Link>
+    <>
+    <header className={classes.header}>
+      <h1>Delicious meals, created{' '}
+        <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+      <p className={classes.cta}>
+        <Link href='/meals/share'>
+          Share Your Favorite Recipe
+        </Link>
       </p>
-      <p>
-        <Link href="/meals/pad-thai">Pad Thai</Link>
-      </p>
-      <p>
-        <Link href="/meals/sloppy-joe">Sloppy Joe</Link>
-      </p>
-    </main>
+      <main className={classes.main}>
+        
+      </main>
+    </header>
+    </>
   );
 };
 
-export default Meals;
+export default MealsPage;

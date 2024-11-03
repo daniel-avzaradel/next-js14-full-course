819d72685d8ac922d2c7cfc3df76e2e5ebae48bd
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-11-03 04:07:57 +0200

    meal not found page

---
+++ 05-onwards-foodies-starting-project/app/meals/not-found.jsx
@@ -0,0 +1,11 @@
+
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>MEAL NOT FOUND</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  )
+}
+
+export default NotFoundPage

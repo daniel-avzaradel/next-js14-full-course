b4b405734e56989ad12e2a211b7c4a7509402a3b
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-11-03 02:31:51 +0200

    fixing styles

--- 05-onwards-foodies-starting-project/app/meals/page.jsx
+++ 05-onwards-foodies-starting-project/app/meals/page.jsx
@@ -1,27 +1,35 @@
+import { Suspense } from "react";
+
 import Link from "next/link";
-import classes from './page.module.css'
 import MealsGrid from "../components/meals/meals-grid";
 
+import classes from './page.module.css'
 import { getMeals } from "@/lib/meals";
 
-const MealsPage = async () => {
-
+const Meals = async () => {
   const meals = await getMeals();
-  
+
+  return(<MealsGrid meals={meals} />)
+}
+
+const MealsPage = () => {
+
   return (
     <>
     <header className={classes.header}>
       <h1>Delicious meals, created{' '}
         <span className={classes.highlight}>by you</span>
       </h1>
-      <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+      <p className={classes.headerp}>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
       <p className={classes.cta}>
         <Link href='/meals/share'>
           Share Your Favorite Recipe
         </Link>
       </p>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </header>
     </>

5d8f8a4cd0623770765e995c9b654bd701736caf
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-11-06 00:49:50 +0200

    updating form button as we submit form with useFormStatus

--- 05-onwards-foodies-starting-project/app/meals/share/page.jsx
+++ 05-onwards-foodies-starting-project/app/meals/share/page.jsx
@@ -1,6 +1,7 @@
 import ImagePicker from '@/app/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '../meals-form-submit';
 
 export default function ShareMealPage() {
 
@@ -43,7 +44,7 @@ export default function ShareMealPage() {
           </p>
             <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>

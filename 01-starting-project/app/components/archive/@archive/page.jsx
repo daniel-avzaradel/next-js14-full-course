40e7c9a2b1ed67b40c1f0030a2a7743b2edc743e
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-11-19 23:44:14 +0200

    parallel pages

---
+++ 01-starting-project/app/components/archive/@archive/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArchivePage = () => {
+  return (
+    <h1>Archive Page</h1>
+  )
+}
+
+export default ArchivePage
---
+++ 01-starting-project/app/components/archive/@latest/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LatestNewsPage = () => {
+  return (
+    <h1>LatestNewsPage</h1>
+  )
+}
+
+export default LatestNewsPage

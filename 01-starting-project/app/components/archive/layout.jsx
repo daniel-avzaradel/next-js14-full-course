5359861a8793eeb9378a3252279c6ed20f17a376
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-11-19 23:46:25 +0200

    layout

--- 01-starting-project/app/components/archive/layout.jsx
+++ 01-starting-project/app/components/archive/layout.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ArchiveLayout = ({archive, latest}) => {
   return (
-    <div>ArchiveLayout</div>
+    <div>
+      <h1>News Archive</h1>
+      <section id="archive-filter">{archive}</section>
+      <section id="archive-latest">{latest}</section>
+    </div>
   )
 }
 

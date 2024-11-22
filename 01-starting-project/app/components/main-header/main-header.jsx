7a4c311c27971cd944a1ed6815c5f96e7b2bafa4
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-11-22 22:40:43 +0200

    fixed navlinks

--- 01-starting-project/app/components/main-header/main-header.jsx
+++ 01-starting-project/app/components/main-header/main-header.jsx
@@ -5,13 +5,21 @@ import classes from "./main-header.module.css";
 import NavLink from "./nav-link";
 
 const MainHeaderComponent = () => {
-
   return (
     <header className={classes.header}>
       <Link href={"/"}>
         <h1>NEXTJS NEWS APP</h1>
       </Link>
-      <NavLink />
+      <nav>
+        <ul>
+          <li>
+            <NavLink href={"news"} />
+          </li>
+          <li>
+            <NavLink href={"archive"} />
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
--- 01-starting-project/app/components/main-header/nav-link.jsx
+++ 01-starting-project/app/components/main-header/nav-link.jsx
@@ -3,34 +3,20 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import classes from './main-header.module.css'
+import classes from "./main-header.module.css";
 
-const NavLink = () => {
-
+const NavLink = ({ href }) => {
   const path = usePathname();
 
   return (
-    <nav>
-      <ul>
-        <li></li>
-        <li>
-          <Link
-            href={"/news"}
-            className={path.startsWith("/news") ? classes.active : undefined}
-          >
-            NEWS
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={"/archive"}
-            className={path.startsWith("/archive") ? classes.active : undefined}
-          >
-            ARCHIVE
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <li>
+      <Link
+        href={href}
+        className={path.startsWith(`/${href}`) ? classes.active : undefined}
+      >
+        {href.toUpperCase()}
+      </Link>
+    </li>
   );
 };
 
